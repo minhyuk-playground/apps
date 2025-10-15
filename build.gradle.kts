c14c4e1f5804849a5851ad9623c5dc3892b120ee
minhyuk yoon <ymh92730@gmail.com>
2025-10-16 06:18:20 +0900

    chore: 의존성 수정 및 그레이들 속성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,19 +27,18 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-batch")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-jooq")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
+//    implementation("org.springframework.boot:spring-boot-starter-batch")
+//    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+//    implementation("org.springframework.boot:spring-boot-starter-jooq")
+//    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    runtimeOnly("com.h2database:h2")
-    runtimeOnly("com.mysql:mysql-connector-j")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+//    runtimeOnly("com.h2database:h2")
+//    runtimeOnly("com.mysql:mysql-connector-j")
+//    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("org.springframework.batch:spring-batch-test")
+//    testImplementation("org.springframework.batch:spring-batch-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,10 @@
 rootProject.name = "apps"
+
+includeBuild("./settings/plugins-spring")
+includeBuild("./settings/infra-spring")
+
+include(
+    ":like:core",
+    ":like:batch",
+    ":like:customer-api",
+)
