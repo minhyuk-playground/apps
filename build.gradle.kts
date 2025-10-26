1b5ff781792aa920a428fc0905c58790ea89e5b4
minhyuk yoon <ymh92730@gmail.com>
2025-10-26 18:17:03 +0900

    chore: root project build.gradle.kts & settings.gradle.kts 수정
    - springLibs 적용 및 like 모듈 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,8 @@
-plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
-    kotlin("plugin.jpa") version "1.9.25"
-    id("org.springframework.boot") version "3.5.6"
-    id("io.spring.dependency-management") version "1.1.7"
-}
 
-group = "me.mh"
+group = "me.mh.apps"
 version = "1.0.0"
 description = "apps"
 
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
-}
-
-configurations {
-    compileOnly {
-        extendsFrom(configurations.annotationProcessor.get())
-    }
-}
 
 repositories {
     mavenCentral()
@@ -31,29 +13,13 @@ dependencies {
 //    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 //    implementation("org.springframework.boot:spring-boot-starter-jooq")
 //    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
+//    implementation("org.springframework.boot:spring-boot-starter-web")
+//    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+//    implementation("org.jetbrains.kotlin:kotlin-reflect")
 //    runtimeOnly("com.h2database:h2")
 //    runtimeOnly("com.mysql:mysql-connector-j")
 //    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+//    testImplementation("org.springframework.boot:spring-boot-starter-test")
 //    testImplementation("org.springframework.batch:spring-batch-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-kotlin {
-    compilerOptions {
-        freeCompilerArgs.addAll("-Xjsr305=strict")
-    }
-}
-
-allOpen {
-    annotation("jakarta.persistence.Entity")
-    annotation("jakarta.persistence.MappedSuperclass")
-    annotation("jakarta.persistence.Embeddable")
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
+//    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,19 @@
 rootProject.name = "apps"
 
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("springConventionLibs") {
+            from(files("./settings/versions/spring/convention.versions.toml"))
+        }
+    }
+}
+
 includeBuild("./settings/plugins-spring")
-includeBuild("./settings/infra-spring")
+includeBuild("./settings/support-spring")
 
 include(
     ":like:core",
-    ":like:batch",
-    ":like:customer-api",
+    ":like:infra:persistence:mysql-like",
+    ":like:apps:batch",
+    ":like:apps:customer-api",
 )
