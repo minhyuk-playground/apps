9221b6ac2927347946914d80d5bf048378c3eb8b
minhyuk yoon <ymh92730@gmail.com>
2025-11-12 00:02:24 +0900

    config(gradle) : update kotlin convention plugin dependency

--- settings/plugins-spring/src/main/kotlin/me/mh/apps/settings/plugins/spring/kotlin.gradle.kts
+++ settings/plugins-spring/src/main/kotlin/me/mh/apps/settings/plugins/spring/kotlin.gradle.kts
@@ -1,9 +1,5 @@
 package me.mh.apps.settings.plugins.spring
 
-import gradle.kotlin.dsl.accessors._160bcc4ee9483fe4d5a2d8de58fe9f02.testImplementation
-import gradle.kotlin.dsl.accessors._160bcc4ee9483fe4d5a2d8de58fe9f02.testRuntimeOnly
-
-
 plugins {
     kotlin("jvm")
     id("org.springframework.boot")
@@ -15,8 +11,8 @@ repositories {
 }
 
 dependencies {
-    // TODO("Junit5 의존성으로 수정")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testImplementation("org.junit.jupiter:junit-jupiter-params")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
