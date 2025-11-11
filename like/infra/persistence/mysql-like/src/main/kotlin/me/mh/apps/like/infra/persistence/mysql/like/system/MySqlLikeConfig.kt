226c6238f2ef00cef96152c1f7e811b2bab68a2c
minhyuk yoon <ymh92730@gmail.com>
2025-11-12 00:38:17 +0900

    refactor(mysql-like): extend abstract configuration templates
    - Updated `MySqlLikeConfig` to implement `HikariReplicationDataSourceConfigurationTemplate` and `JpaConfigurationTemplate`.
    - Simplified configuration methods by overriding template methods.
    - Improved consistency and modularity in persistence layer setup.

--- like/infra/persistence/mysql-like/src/main/kotlin/me/mh/apps/like/infra/persistence/mysql/like/system/MySqlLikeConfig.kt
+++ like/infra/persistence/mysql-like/src/main/kotlin/me/mh/apps/like/infra/persistence/mysql/like/system/MySqlLikeConfig.kt
@@ -1,9 +1,11 @@
 package me.mh.apps.like.infra.persistence.mysql.like.system
 
 import com.zaxxer.hikari.HikariDataSource
+import jakarta.persistence.EntityManagerFactory
 import me.mh.apps.like.infra.persistence.mysql.like.system.constants.MySqlLikeConstants
+import me.mh.apps.settings.infra.spring.persistence.datasource.HikariReplicationDataSourceConfigurationTemplate
 import me.mh.apps.settings.infra.spring.persistence.datasource.ReplicationRoutingDataSource
-import me.mh.apps.settings.infra.spring.persistence.jpa.system.EntityManagerFactoryConfiguration
+import me.mh.apps.settings.infra.spring.persistence.jpa.system.JpaConfigurationTemplate
 import me.mh.apps.settings.infra.spring.persistence.jpa.system.properties.JpaCustomProperties
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
@@ -15,49 +17,48 @@ import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy
 import org.springframework.orm.jpa.JpaTransactionManager
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
 import org.springframework.transaction.PlatformTransactionManager
+import javax.sql.DataSource
 
 @Configuration
 @ConfigurationPropertiesScan(MySqlLikeConstants.BASE_PACKAGE)
-internal class MySqlLikeConfig {
+internal class MySqlLikeConfig :
+    HikariReplicationDataSourceConfigurationTemplate,
+    JpaConfigurationTemplate {
+
+    override val packageToScan: String = MySqlLikeConstants.BASE_PACKAGE
+    override val persistenceUnit: String = MySqlLikeConstants.PERSISTENCE_UNIT_NAME
 
     @Bean(MySqlLikeConstants.PRIMARY_DATA_SOURCE_NAME)
     @ConfigurationProperties("like.data-sources.replication.hikari.primary")
-    fun likeMySqlPrimaryDataSource(): HikariDataSource = HikariDataSource()
+    override fun primary(): HikariDataSource = super.primary()
 
     @Bean(MySqlLikeConstants.SECONDARY_DATA_SOURCES_NAME)
     @ConfigurationProperties("like.data-sources.replication.hikari.secondaries")
-    fun likeMySqlSecondaryDataSources(): List<HikariDataSource> = mutableListOf()
+    override fun secondary(): HikariDataSource = super.secondary()
 
     @Bean(MySqlLikeConstants.REPLICATION_DATA_SOURCE_NAME)
-    fun likeMySqlReplicationDataSource(): ReplicationRoutingDataSource =
-        ReplicationRoutingDataSource(
-            primaryId = likeMySqlPrimaryDataSource().poolName,
-            dataSources = likeMySqlSecondaryDataSources().associateBy { it.poolName }
-        )
+    override fun replicationDataSource(): ReplicationRoutingDataSource = super.replicationDataSource()
 
     @Bean(name = [MySqlLikeConstants.DATA_SOURCE_NAME, "datasource"])
     @Primary
-    fun likeMySqlDataSource(): LazyConnectionDataSourceProxy =
-        LazyConnectionDataSourceProxy(likeMySqlReplicationDataSource())
+    override fun dataSource(): LazyConnectionDataSourceProxy = super.dataSource()
 
     @Bean
     @ConfigurationProperties("like.jpa")
-    fun likeJpaCustomProperties(): JpaCustomProperties = JpaCustomProperties.mysql()
+    override fun jpaCustomProperties(): JpaCustomProperties = JpaCustomProperties.mysql()
 
     @Bean(MySqlLikeConstants.ENTITY_MANAGER_FACTORY_NAME)
-    fun likeEntityManagerFactory(): LocalContainerEntityManagerFactoryBean =
-        EntityManagerFactoryConfiguration(
-            dataSource = likeMySqlReplicationDataSource(),
-            packageToScan = MySqlLikeConstants.BASE_PACKAGE,
-            persistenceUnit = MySqlLikeConstants.PERSISTENCE_UNIT_NAME,
-            jpaCustomProperties = likeJpaCustomProperties()
-        ).toEntityManagerFactoryBean()
+    override fun entityManagerFactory(dataSource: DataSource): LocalContainerEntityManagerFactoryBean =
+        super.entityManagerFactory(dataSource)
 
     @Bean(name = [MySqlLikeConstants.TX_MANAGER_NAME, "transactionManager"])
-    fun likeTransactionManager(): PlatformTransactionManager =
-        JpaTransactionManager(likeEntityManagerFactory().`object`!!)
+    @Primary
+    override fun jpaTransactionManager(
+        entityManagerFactory: EntityManagerFactory
+    ): JpaTransactionManager =
+        super.jpaTransactionManager(entityManagerFactory)
 
     @Bean(MySqlLikeConstants.JDBC_TEMPLATE_NAME)
     fun likeJdbcTemplate(): NamedParameterJdbcTemplate =
-        NamedParameterJdbcTemplate(likeMySqlDataSource())
+        NamedParameterJdbcTemplate(dataSource())
 }
